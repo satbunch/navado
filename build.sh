12fe5753247886b15e6f575fb5e913529bd721b6
Satomi Yoshio <satomi.yoshio@diamond-f.co.jp>
2025-12-04 14:58:57 +0900

    chore: version up -> 1.0.1 and create directry dist/icons/

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf dist
-mkdir -p dist/js dist/options
+mkdir -p dist/js dist/options dist/icons
 
 cp manifest.json dist/
 cp content.js dist/
@@ -9,6 +9,7 @@ cp popup.html dist/
 cp popup.js dist/
 cp js/*.js dist/js/
 cp options/* dist/options/
+cp icons/* dist/icons/
 
 cd dist
 zip -r ../Navado-${1}.zip .
